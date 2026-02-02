04166e939e593a7c3fcacdc86aa79a967e9e10e3
Ayomide Olusesan <kidd37214@gmail.com>
2026-02-02 09:00:44 +0100

    feat(prisma): add initial Prisma configuration and schema files
    - Created prisma.config.ts for Prisma configuration with datasource URL from environment variables.
    - Added schema.prisma defining the PostgreSQL datasource and client generator.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Biometric Voting System API')
+    .setDescription('API for managing biometric voting system')
+    .setVersion('1.0')
+    .addTag('biometric-voting')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger UI is available at: ${await app.getUrl()}/api`);
 }
 bootstrap();
